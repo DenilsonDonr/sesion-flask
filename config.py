d9566fd05ab9f98c326fc6ecd3f89056ad25141d
Denilson <1476037@senati.pe>
2023-10-20 22:11:34 -0500

    Conexión a DB y creación de Token para sesión

---
+++ config.py
@@ -0,0 +1,10 @@
+#Conexion a la base de datos, referencia a mysqlhostingnet
+
+MYSQL_HOST = "sql10.freemysqlhosting.net"
+MYSQL_USER = "sql10653875"
+MYSQL_PASSWORD = "XR2L64G71S"
+MYSQL_DB = "sql10653875"
+
+#Creación de token para la sesión
+
+HEX_SEC_KEY = "d87b9v73c0a9b8208as9z0b8xc00a8x4e"
