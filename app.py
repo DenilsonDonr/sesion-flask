ea5e004a53fb76c50b75578451944a8f124541f4
Denilson <1476037@senati.pe>
2023-10-21 15:52:03 -0500

    actualizacion de app.py, se integró config para la futura conexión a la DB, también se realizó el proceso para login, def login

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 #importaciones
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import config
 #app archivo principal de la aplicación
 #instancia de Flask
 
 app = Flask(__name__)
 
+#configuracion de la base de datos
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY
+app.config['MYSQL_HOST'] = config.MYSQL_HOST
+app.config['MYSQL_USER'] = config.MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config.MYSQL_DB
+
+
+
 #ruta principal y metodo
 @app.route('/', methods=['GET'])
 def home():
+    #render_template, necesario para renderizar un .html
     return render_template('index.html')
 
+#Ruta para realizar el proceso de login
+@app.route('/login', methods=['POST'])
+def login():
+    #recibir email y password
+    email = request.form['email']
+    password = request.form['password']
 
 if __name__ == '__main__':
     app.run(debug=True)
